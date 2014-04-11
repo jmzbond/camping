6ffc09b450252f805378e0718249691b5bf534e7
James Dong <jdong8@gmail.com>
2014-04-11 15:58:48 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Camping::Application.config.secret_key_base = '9f3f0d7bc9d3bb4056d73eee352d8671ab2524645e21470d412a15549ab5678638927f4aece0a95bb4116d2a3294b77fe9feb505c262397d8f4b86c4358ad2f6'
